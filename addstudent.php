f8a66437363db138282d6442c715119f519c5585
Muhammad Noman. Siddiqui <nomansiddiqui@aptech.com>
2026-09-17 12:33:41 +0500

    addstudent.php/viewstudent.php/githubcommmand.txtfile

--- addstudent.php
+++ addstudent.php
@@ -5,6 +5,7 @@ if($_SERVER['REQUEST_METHOD']==='POST')
 {
    $name=$_POST['name'];
    $email=$_POST['email'];
+  
 
    $query= "INSERT INTO student (`name`, `email`) VALUES ('$name','$email')";
    $sql=mysqli_query($connection, $query);
@@ -24,15 +25,29 @@ if($_SERVER['REQUEST_METHOD']==='POST')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
     <title>Document</title>
 </head>
 <body>
+<center> <h1>ADD STUDENT</h1></center>
 <form action="" method="post">
-     <label for="">Enter Name</label><br>
-     <input type="text"  name="name" placeholder="Enter name"><br>
-     <label>Enter Email</label><br>
-     <input type="text"  name="email" placeholder="Enter Email"/>
+     <label for=""  class="form-label">Enter Name</label><br>
+     <input type="text"  name="name" class="form-control" placeholder="Enter name"><br>
+     <label class="form-label">Enter Email</label><br>
+     <input type="text" class="form-control"  name="email" placeholder="Enter Email"/>
+   <label class="form-label">Enter Phone No</label><br>
+     <input type="text" class="form-control"  name="email" placeholder="Enter phone numer"/>
+        <label class="form-label">Enter cnic</label><br>
+     <input type="text" class="form-control"  name="email" placeholder="Enter cnic"/>
+             <label class="form-label">Enter Gender</label><br>
+             <select name="" id="">
+                    <option value="">select Gender</option>
+              <option value="">Male</option>
+              <option value="">Female</option>
+             </select>
      <button type="submit">Add student</button>
 </form>
+
 </body>
 </html>
---
+++ viewstudent.php
@@ -0,0 +1,47 @@
+<?php
+  include("connection.php");
+ $sql="SELECT * FROM student";
+$result=$connection->query($sql);
+// $row=$result->fetch_assoc();
+// echo $row['id'];
+// echo $row['name'];
+// echo $row['email'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1>View Student</h1>
+    <table class="table"> 
+        <tr>
+            <th scope="col">Id</th>
+            <th  scope="col">Name</th>
+            <th scope="col">Email</th>
+             <th scope="col">Action</th>
+        </tr>
+<?php if($result->num_rows>0):?>
+     <?php while($row=$result->fetch_assoc()):  ?>
+        <tr scope="row">
+      <td><?php echo $row['id'] ?></td>
+      <td><?php echo  $row['name']?></td>
+      <td><?php echo $row['email'] ?></td>
+      <td>
+    <a href="" class="btn btn-primary">Update</a>
+        <a href="" class="btn btn-danger">delete</a>
+     </td>
+     </tr>
+     <?php endwhile;?>
+     <?php else: ?>
+        <tr>
+     <td colspan="3" >No student found</td>
+     </tr>
+     <?php endif; ?>
+    </table>
+</body>
+</html>
